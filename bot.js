375060e05dfd8f23e78c7ba4982eb51db8d60753
iiUMeex <41113180+iiUMeex@users.noreply.github.com>
2019-01-25 23:12:52 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix ="-"
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -23,7 +22,7 @@ const Discord = require("discord.js");
         });
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = "$";
+var prefix = "-";
 client.on("message", message => {
 
             if (message.content.startsWith(prefix + "bc")) {
