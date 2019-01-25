115e3ffe8a0e959e02afd09e96fb251438118eca
iiUMeex <41113180+iiUMeex@users.noreply.github.com>
2019-01-25 22:57:28 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,15 +19,48 @@ client.on('ready', () => {
 client.user.setGame(`DARK SIDE SERVER'); 
  });
 
-client.on('message', msg =>{
-    let message=msg;
-    if(message.content.startsWith("-bc")){
-        var args = message.content.split(' ').slice(1).join(' ');
-    msg.guild.members.forEach(m=>{
-        m.send(args.replace(/[user]/g,m)).catch();
-    if(message.attachments.first()){
-m.sendFile(message.attachments.first().url).catch();
-    }
-    })    ;
-    }
+const Discord = require("discord.js");
+  const client = new Discord.Client();
+      kboosh.on('ready', () => {
+        kboosh.user.setGame(`DARKSIDE SERVER`,'https://www.twitch.tv/TEST-Broadcast');
+          console.log('Im Ready!');
+  
+        });
+
+  client.on('message', message => {
+    if (message.content.split(' ')[0] == '-bc')
+       message.guild.members.forEach( member => {
+         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+
+
+           member.send( `${member} ! ` + "**" + message.guild.name + " : ** " + message.content.substr(3));
+                                                      message.delete();
+            
+                                                    });
+            
+                                                  });
+   client.on("message", message => {
+
+             var args = message.content.substring(prefix.length).split(" ");
+                if (message.content.startsWith(prefix + "b")) {
+                          if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+
+                          if (!args[1]) {
+                            
+                                 let embed3 = new Discord.RichEmbed()
+                                     .setDescription(":white_check_mark: | تم ارسال رسالة لا يوجد فيها شيء")
+                                       .setColor("#FF00FF")
+                                          message.channel.sendEmbed(embed3);
+                            
+                                        } else {
+
+                            
+                                           let embed4 = new Discord.RichEmbed()
+                                                            .setDescription(':white_check_mark: | تم ارسال الرساله للجميع ..')
+                                                                .setColor("#99999")
+                               
+                                                                message.channel.sendEmbed(embed4);
+                                                      message.delete();
+                            }
+                          }
 });
