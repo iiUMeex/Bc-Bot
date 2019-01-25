5f0379a667eefcb8652f00020b8732067e75f3be
iiUMeex <41113180+iiUMeex@users.noreply.github.com>
2019-01-25 23:20:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -36,15 +36,3 @@ client.on("message", message => {
  message.delete(); 
 };     
 });
-
-
-client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Desert Bot- Script By : i1Suhaib`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`DARKSIDE SERVER`,"http://twitch.tv/S-F")
-client.user.setStatus("dnd")
-});
