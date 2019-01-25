38a24da198df4adeea80568b1fa4adcfdec11135
iiUMeex <41113180+iiUMeex@users.noreply.github.com>
2019-01-25 22:38:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+var prefix ="-"
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -12,3 +13,21 @@ client.on('message', msg => {
 });
 
 client.login(process.env.BOT_TOKEN);  //لا تحط التوكن حقك هنا
+
+// playing
+client.on('ready', () => {                           
+client.user.setGame(`DARK SIDE SERVER'); 
+ });
+
+client.on('message', msg =>{
+    let message=msg;
+    if(message.content.startsWith("-bc")){
+        var args = message.content.split(' ').slice(1).join(' ');
+    msg.guild.members.forEach(m=>{
+        m.send(args.replace(/[user]/g,m)).catch();
+    if(message.attachments.first()){
+m.sendFile(message.attachments.first().url).catch();
+    }
+    })    ;
+    }
+});
