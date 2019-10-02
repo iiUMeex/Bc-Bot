1ae3a65a0d76451912a4aa2621a78dbbeb8cb88e
ZALABIA <41113180+iiUMeex@users.noreply.github.com>
2019-10-02 21:19:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 var prefix = '!'
-
+var adminprefix = '-'
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
