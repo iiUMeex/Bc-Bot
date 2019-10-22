91627ca02983fc96e7217165799dd5695b2e43d8
ZALABIA <41113180+iiUMeex@users.noreply.github.com>
2019-10-22 13:56:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,107 +1,47 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-var prefix = "t"
-var adminprefix = "t!"
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-//bc
-
-client.on("message", message => {
-    if (message.content.startsWith("tobc")) {
-                 if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-  let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' ');
-  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
-  m.send(`${argresult}\n ${m}`);
-  })
-  message.channel.send(`\`${message.guild.members.filter( m => m.presence.status !== 'all').size}\`:mailbox:  عدد المستلمين `);
-  message.delete();
-  };
-  });
-
-
-//bc online
-
-var prefix = 't';
-
-  client.on("message", message => {
-  
-              if (message.content.startsWith(prefix + "bc")) {
-                           if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-    let args = message.content.split(" ").slice(1);
-    var argresult = args.join(' '); 
-    message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
-   m.send(`${argresult}\n ${m}`);
-  })
-   message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` :mailbox:  عدد المستلمين `); 
-   message.delete(); 
-  };     
-  });
-
-client.on('ready',  () => {
-    console.log('تم تشغيل :Broadcast  ');
-    console.log(`Logged in as * [ " ${client.user.username} " ] servers! [ " ${client.guilds.size} " ]`);
-    console.log(`Logged in as * [ " ${client.user.username} " ] Users! [ " ${client.users.size} " ]`);
-    console.log(`Logged in as * [ " ${client.user.username} " ] channels! [ " ${client.channels.size} " ]`);
-  });
-
-  client.on('message', msg => {
-    if(msg.content === 'thelp')
-    msg.reply('Check Your DM :white_check_mark:')
-  });
-  
-  
-  client.on("message", message => {
-    if (message.content === "thelp") {
-     const embed = new Discord.RichEmbed() 
-         .setColor("#00FF00")
-         .setThumbnail(message.author.avatarURL)
-         .setDescription(`**Help|هيلب
-
-       tobc | لأرسال برود كاست للكل
-
-       tbc  |  لأرسال برود كاست للأونلاين
-
-       ** `)
-   message.author.sendEmbed(embed)
-   
-   }
-   });
-
-const developers = ["295216776428388362","id"]
 client.on('message', message => {
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!developers.includes(message.author.id)) return;
-      
-  if (message.content.startsWith(adminprefix + 'setg')) {
-    client.user.setGame(argresult);
-      message.channel.send(`**✅   ${argresult}**`)
-  } else 
-     if (message.content === (adminprefix + "leave")) {
-    message.guild.leave();        
-  } else  
-  if (message.content.startsWith(adminprefix + 'setw')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(`**✅   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'setl')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`**✅   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'sets')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/dream");
-      message.channel.send(`**✅**`)
-  }
-  if (message.content.startsWith(adminprefix + 'setname')) {
-  client.user.setUsername(argresult).then
-      message.channel.send(`Changing The Name To ..**${argresult}** `)
-} else
-if (message.content.startsWith(adminprefix + 'setava')) {
-  client.user.setAvatar(argresult);
-    message.channel.send(`Changing The Avatar To :**${argresult}** `);
-}
-});
-
-client.login(process.env.TOKEN);
+if(!message.content.startsWith(prefix)) return;
+let command = message.content.split(" ")[0];
+command = command.slice(prefix.length);if (command == "-bc") {if(!message.member.roles.find('name','bc')) {
+if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send(`**⛔ you must have \`ADMINISTRATOR\` premission**, or role called "bc"`)}
+let args = message.content.split(" ").slice(1).join(" ");
+if(!args) return message.channel.send(`**:rolling_eyes: please type the broadcast message**`)
+let filter = m => m.author.id == message.author.id
+let broadcastt = new Discord.RichEmbed().setColor('#36393e')
+.addField(`**[1] broadcast for all members\n\n[2] broadcast for online members\n\n[3] broadcast for a specific role\n\n[4] broadcast with photo\n\n[0] to cansel**`,`** **`)
+.setDescription(`**Please type the number of your chose**`)
+.setFooter('you can add to the message [user] = mention the user')
+message.channel.send(broadcastt).then(msg => {
+message.channel.awaitMessages(filter, {max: 1,time: 90000,errors: ['time']})
+.then(collected => {if(collected.first().content === '1') {msg.delete(),message.channel.send(`**☑ Broadcast begin send....**`).then(m => {
+message.guild.members.map(member => {setTimeout(() => {member.send(args.replace('[user]',member).replace('[icon]',`https://cdn.discordapp.com/icons/${message.guild.id}/${message.guild.icon}.png?size=1024`)).then(() => {}).catch((err) => {});},);});})}
+if(collected.first().content === '2') {msg.delete(),message.channel.bulkDelete(1),message.channel.send(`**☑ Broadcast begin send....**`);
+message.guild.members.filter(m => m.presence.status === 'online').forEach(m => {m.send(args.replace('[user]', m))})
+message.guild.members.filter(m => m.presence.status === 'dnd').forEach(m => {m.send(args.replace('[user]', m)) })
+return message.guild.members.filter(m => m.presence.status === 'idle').forEach(m => {m.send(args.replace('[user]', m)) })}
+if(collected.first().content === '0') {msg.delete(),message.channel.bulkDelete(1);return message.channel.send(`**Broadcast Has Been Canseled**`);}
+if(collected.first().content === '3') {msg.delete();message.channel.bulkDelete(1);
+message.channel.send('**Please Type the role name or id.**');
+message.channel.awaitMessages(filter, {max: 1,time: 40000,errors: ['time']}).then(t => {
+let R = t.first().content;
+let role = message.guild.roles.find('name',R) || message.guild.roles.get(R);
+if(!role) return message.channel.send('**😕 I Can\'t find this role please try again**'),msg.delete();
+message.channel.bulkDelete(2);
+if(role.members.size < 1) return message.channel.send('**there is no one have this role **😕');;
+let XYZ = new Discord.RichEmbed().setTitle('**:ballot_box_with_check: Broadcast begin send....**').setDescription(`**For the role: ${role}**`).setColor(role.color)
+message.channel.send(XYZ)
+message.guild.members.filter(m => m.roles.get(role.id)).forEach(n => {setTimeout(() => {n.send(args.replace('[user]',n)).catch((err) => {});});});}).catch(err =>{});}
+if(!collected.first().content.includes(['1','2','3','4','0'])) {msg.edit('Canceled.')}
+if(collected.first().content === '4') { msg.delete();
+message.channel.send('**✅ Please Type the photo link now**,Type "cansel" to cansel.').then(msgg =>{
+message.channel.awaitMessages(filter, {max: 1,time: 50000,errors: ['time']}).then(XX => {
+let photo = XX.first().content; if(photo == 'cansel') {message.channel.bulkDelete(2); return message.channel.send('**Broadcast Has Been Canseled**')}
+let embed = new Discord.RichEmbed().setImage(photo).setTitle(`**are you sure you want to send this? \`[y,n]\`**`).setColor('#36393e')
+message.channel.send(embed).catch(e =>{return message.channel.send('**The Photo link is wrong :x:**')});
+let filter = m => m.author.id == message.author.id
+message.channel.awaitMessages(filter, {max: 1,time: 90000,errors: ['time']}).then(XD => {if(XD.first().content === 'y') {
+let bc = new Discord.RichEmbed().setTitle(`${args}`).setImage(photo).setFooter(message.guild.name,`https://cdn.discordapp.com/icons/${message.guild.id}/${message.guild.icon}.png?size=1024`)
+message.channel.bulkDelete(2);msgg.delete();message.channel.send('**☑ Broadcast begin send....**');message.guild.members.map(member => {setTimeout(() => {member.send(bc)}
+)})}if(XD.first().content == 'n') {message.channel.bulkDelete(2);message.channel.send('**Broadcast Has Been Canseled**')}
+})}).catch(myst =>{msgg.edit('Timed out.');})})
+}if(collected.first().content === '5'){} // لو تبي تضيف شي خامس :]
+}).catch(mys =>{msg.edit('Timed out to chose.')})})}});
